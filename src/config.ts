8313d8f1bec31d3b2305f70d1839b4637acacaba
Hakoke <ynkk46i2@example.com>
2025-10-31 21:31:15 +0400

    Add config.ts file with default values for deployment

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export default {
+	CODE_PREFIX: "monopoly-game-prefix", // required - change this to your desired prefix
+	PEER_SERVER_HOST: undefined as string | undefined, // optional
+	PEER_SERVER_PORT: 443 as number | undefined, // optional, default 443
+	PEER_SECURE: false as boolean | undefined, // optional, default false
+	PEER_DEBUG_LEVEL: 0 as number | undefined, // optional, default 0
+};
+
